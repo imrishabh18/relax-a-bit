07596fbe9d59b37c7edf06a7741e59231516b1b5
Rishabh Gupta <rishabhgupta@Rishabhs-MBP.Dlink>
2021-06-27 08:07:48 +0530

    First version

---
+++ index.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+const brightness = require('brightness');
+const syncExec = require('sync-exec');
+
+const command = process.argv.slice(2).join(' ');
+// console.log(command)
+
+var initialBrightness
+
+brightness.get().then(level => {
+	initialBrightness = level
+    brightness.set(0)
+    syncExec(command, {stdio: [0, 1, 2]})
+});
+
+
+const restoreBrightness = () => {
+    brightness.set(initialBrightness)
+    process.exit()
+};
+
+process.on('exit',restoreBrightness);
